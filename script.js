6c297e2c64495b80c132840d6c8d2fead1f5ac14
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-11-19 13:20:38 +0530

    add js on click btn progress bar will move

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+let bar = document.querySelector(".bar");
+let percentage = document.querySelector(".percentage");
+let btn = document.querySelector("button");
+
+let counter = 0;
+let interval;
+
+btn.addEventListener('click', function() {
+    
+
+    interval = setInterval(()=>{
+        counter++
+        bar.style.width = counter + '%';
+        bar.style.backgroundColor = '#469110'
+
+        percentage.innerHTML = counter+'%'
+    }, 50)
+
+    setTimeout(()=>{
+        clearInterval(interval);
+    }, 5000)
+})
