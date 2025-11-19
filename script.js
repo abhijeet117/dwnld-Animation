b819ec0b65e1cc2629a42d59fd72a770b21dd252
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-11-19 13:28:06 +0530

    added feature to again download from 0

--- script.js
+++ script.js
@@ -8,16 +8,25 @@ let interval;
 
 btn.addEventListener('click', function() {
     
+    counter = 0;
+    clearInterval(interval);
+    bar.style.width = '0%';
+    percentage.innerHTML = '0%'
+    
 
     interval = setInterval(()=>{
         counter++
         bar.style.width = counter + '%';
         bar.style.backgroundColor = '#469110'
 
-        percentage.innerHTML = counter+'%'
+        percentage.innerHTML = counter+'%';
+
+        if(counter >= 100) {
+            clearInterval(interval);
+        }
     }, 50)
 
-    setTimeout(()=>{
-        clearInterval(interval);
-    }, 5000)
+    // setTimeout(()=>{
+    //     clearInterval(interval);
+    // }, 5000)
 })
